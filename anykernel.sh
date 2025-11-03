49beb47d43bec1cc74ad2b652c7413049342ee49
Lastpickle <bystanderguts@gmail.com>
2025-11-03 16:00:35 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string= Lineage Kernel By @BystanderforU
+kernel.string= miku Kernel By @BystanderforU
 do.devicecheck=1
 do.modules=0
 do.systemless=1
